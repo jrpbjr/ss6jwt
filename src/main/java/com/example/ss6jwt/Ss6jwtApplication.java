f6744e48e4fdffa7932129fc2b2f3ee00a0ed4de
jrpbjr <jrpbjr@gmail.com>
2024-05-20 09:42:32 -0300

    first commit

---
+++ src/main/java/com/example/ss6jwt/Ss6jwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.ss6jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ss6jwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ss6jwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ss6jwt/Ss6jwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ss6jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ss6jwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
