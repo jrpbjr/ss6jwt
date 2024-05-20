134bc788d719f33f036893aef05555be32d19d0d
jrpbjr <jrpbjr@gmail.com>
2024-05-20 12:17:36 -0300

    services de autenticacao, jwt, detalhes do servico do usuario e login.

---
+++ src/main/java/com/example/ss6jwt/services/DatabaseService.java
@@ -0,0 +1,35 @@
+package com.example.ss6jwt.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.example.ss6jwt.entities.Person;
+import com.example.ss6jwt.enums.Role;
+
+@Service
+public class DatabaseService {
+
+    @Autowired
+    private PersonService personService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void initializeDatabase() {
+
+        System.out.println("Initializing database...");
+
+        final Person user1 = new Person("Emma", "emma@mail.com", passwordEncoder.encode("111"));
+        final Person user2 = new Person("Jhon", "jhon@mail.com", passwordEncoder.encode("222"));
+        final Person admin = new Person("Anna", "anna@mail.com", passwordEncoder.encode("333"));
+
+        admin.addRole(Role.ADMIN);
+
+        System.out.println(personService.create(user1));
+        System.out.println(personService.create(user2));
+        System.out.println(personService.create(admin));
+
+        System.out.println("Database initialized!");
+    }
+}
---
+++ src/main/java/com/example/ss6jwt/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.ss6jwt.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private PersonService personService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        try {
+            return personService.findByEmail(username);
+        }
+        catch (Exception e) {
+            throw new UsernameNotFoundException(e.getMessage());
+        }
+    }
+
+}
